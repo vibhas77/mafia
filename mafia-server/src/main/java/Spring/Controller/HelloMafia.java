0c8ac639a267cc8ec2e637616de4c0bda016d98e
Vibhas Goyal <vibhas777@gmail.com>
2019-08-06 17:49:37 +0530

    major

---
+++ mafia-server/src/main/java/Spring/Controller/HelloMafia.java
@@ -0,0 +1,16 @@
+package Spring.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloMafia {
+
+    @RequestMapping("/hello")
+    public String helloSpring(String[] args) {
+
+        return "Hello Spring";
+
+    }
+
+}
